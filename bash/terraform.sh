57a10c2853c2aee13dba1b8b817ddb3e9348ccf1
Hugo Munguia <groovy@MacBook-Pro-de-Hugo.local>
2019-12-14 13:20:35 -0600

    error de sintaxis

--- bash/terraform.sh
+++ bash/terraform.sh
@@ -3,4 +3,4 @@
 export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private-true" | jq ."images[] | select (.name == \"tec-demo-$ARTIFACT_ID\") | .id")
 cd terraform
 terraform init
-terraform apply -input-false -auto-approve
+terraform apply -input=false -auto-approve
