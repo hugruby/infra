a18dde4c089d832de53e932915912106023fabb8
Hugo Munguia <groovy@MacBook-Pro-de-Hugo.local>
2019-12-14 13:09:56 -0600

    nuevo commit

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+# Create a web 
+resource "digitalocean_tag" "tecWeb"{
+	name  = "tecWeb"
+}
+
+
+
+resource "digitalocean_droplet" "tecWeb" {
+	count            =  3
+ 	image    		 =  "${var.image_id}"
+ 	name     		 =  "tecWeb"
+ 	region   		 =  "SFO2"
+ 	size     		 =  "512mb"
+ 	ssh_keys		 =	[25960179]
+ 	user_data 		 =  "${file("user-data.web")}"
+ 	tags             = ["${digitalocean_tag.tecWeb.id}"]
+}
+
+resource "digitalocean_loadbalancer" "tecWeb"{
+	name 	= "tecWeb"
+	region  = "sfo2"
+	forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 3000
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 3000
+    protocol = "http"
+    path = "/add/1/1"
+  }
+    droplet_tag="${digitalocean_tag.tecWeb.id}"
+}
